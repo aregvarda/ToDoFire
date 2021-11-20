d3d320934da148ac60faab6ad18872993f6316a4
Areg Vardanian <aregv@bk.ru>
2021-11-20 17:08:42 +0300

    second run

--- ToDoFire/Controllers/TasksViewController.swift
+++ ToDoFire/Controllers/TasksViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
@@ -16,6 +18,9 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = .white
         return cell
     }
     
@@ -24,6 +29,8 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
     }
     
 
