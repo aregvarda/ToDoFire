9611bda75040e41fa8caa69841e790c820f0751c
Areg Vardanian <aregv@bk.ru>
2021-11-21 17:40:20 +0300

    finish

--- ToDoFire/Controllers/TasksViewController.swift
+++ ToDoFire/Controllers/TasksViewController.swift
@@ -8,11 +8,15 @@
 import UIKit
 import Firebase
 
-class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class TasksViewController: UIViewController {
     
     var user: User!
     var ref: DatabaseReference!
-    var tasks = Array<Task>()
+    var tasks = [Task]()
+
+    
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,50 +44,9 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         ref.removeAllObservers()
     }
     
-    @IBOutlet weak var tableView: UITableView!
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tasks.count
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        cell.backgroundColor = .clear
-        let task = tasks[indexPath.row]
-        cell.textLabel?.textColor = .white
-        let taskTitle = task.title
-        let isCompleted = task.completed
-        cell.textLabel?.text = taskTitle
-        toggleCompletion(cell, isCompleted: isCompleted)
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            let task = tasks[indexPath.row]
-            task.ref?.removeValue()
-        }
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let cell = tableView.cellForRow(at: indexPath) else { return }
-        let task = tasks[indexPath.row]
-        let isCompleted = !task.completed
-        
-        toggleCompletion(cell, isCompleted: isCompleted)
-        task.ref?.updateChildValues(["completed": isCompleted])
-    }
-    
-    func toggleCompletion(_ cell: UITableViewCell, isCompleted: Bool) {
-        cell.accessoryType = isCompleted ? .checkmark : .none
-    }
-
     @IBAction func addTapped(_ sender: UIBarButtonItem) {
-        
         let alertController = UIAlertController(title: "New task", message: "Add new task", preferredStyle: .alert)
         alertController.addTextField()
         let save = UIAlertAction(title: "Save", style: .default) { [weak self] action in
@@ -99,7 +62,10 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         alertController.addAction(cancel)
         present(alertController, animated: true)
     }
+        
+        
     @IBAction func signOutTapped(_ sender: UIBarButtonItem) {
+       
         do {
             try Auth.auth().signOut()
         } catch {
@@ -108,5 +74,44 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         dismiss(animated: true, completion: nil)
     }
     
+    func toggleCompletion(_ cell: UITableViewCell, isCompleted: Bool) {
+        cell.accessoryType = isCompleted ? .checkmark : .none
+    }
+    
+}
 
+extension TasksViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .clear
+        cell.textLabel?.textColor = .white
+        let currentTask = tasks[indexPath.row]
+        cell.textLabel?.text = currentTask.title
+        toggleCompletion(cell, isCompleted: currentTask.completed)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle != .delete { return }
+            let task = tasks[indexPath.row]
+            task.ref?.removeValue()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        let task = tasks[indexPath.row]
+        let isCompleted = !task.completed
+        
+        toggleCompletion(cell, isCompleted: isCompleted)
+        task.ref?.updateChildValues(["completed": isCompleted])
+    }
+    
 }
---
+++ ToDoFire/Helpers/TDLConstants.swift
@@ -0,0 +1,14 @@
+//
+//  TDLConstants.swift
+//  ToDoFire
+//
+//  Created by Геворг on 21.11.2021.
+//
+
+import Foundation
+
+struct TDLConstants {
+    struct Segues {
+        static let tasks = "tasksSegue"
+    }
+}
